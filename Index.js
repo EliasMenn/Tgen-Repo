0c1617762e95623b188c7237a4a1e7929b70604d
EliasMenn <eliasmennella10@gmail.com>
2022-10-04 16:34:44 -0300

    bootstrap

---
+++ Index.js
@@ -0,0 +1,24 @@
+
+let nombre = prompt("¿Cual es su nombre?");
+alert("Bienvenido " + nombre + "!");
+let edad = prompt("¿Cual es su edad?")
+let edadD = edad * 365
+let edadH = edadD * 24
+let edadM = edadH * 60
+let edadS = edadM * 60
+alert("usted tiene " + edad + " años de edad, lo que significa que usted tiene " + edadD + " dias de edad, o " + edadH + " horas de edad, o " + edadM + 
+" minutos de edad, o " + edadS + " segundos de edad")
+// let valorI = prompt("Ingrese valor 1")
+// let valorII = prompt("Ingrese valor 2")
+// let operacion= prompt("Ingrese la operacion (+ para suma, - para resta, * para multiplicacion, / para division)")
+// if (operacion = "+")
+    // alert(parseInt(valorI)+parseInt(valorII))
+// else if (operacion = "-")
+    // alert(parseInt(valorI)-parseInt(valorII))
+// else if (operacion = "*")
+    // alert(parseInt(valorI)*parseInt(valorII))
+// else if (operacion = "/")
+    // alert(parseInt(valorI)/parseInt(valorII))
+// else 
+    // alert("Ingrese una operacion valida");
+
